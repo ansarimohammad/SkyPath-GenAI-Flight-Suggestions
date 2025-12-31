d1fca8f5b3acbd9003d78b6eb27a58e06a554966
Mohammad Ahmad Ansari <ansari101mohammad@gmail.com>
2026-01-01 00:12:36 +0530

    Update authController.js

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -48,8 +48,8 @@ export const login = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "Strict",
+      secure: true,
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
